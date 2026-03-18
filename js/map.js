5c4bb0fb3a9f3233651f4c51cd9848b9635f331c
Paul Coulter <paulhcoulter@gmail.com>
2026-03-17 21:35:43 -0400

    feat: switch to Stadia AlidadeSmoothDark tile layer

--- js/map.js
+++ js/map.js
@@ -30,9 +30,9 @@ function initMap() {
     zoomControl: true
   });
 
-  L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/">CARTO</a>',
-    maxZoom: 19
+  L.tileLayer("https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png", {
+    attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    maxZoom: 20
   }).addTo(map);
 
   markerLayer = L.layerGroup().addTo(map);
