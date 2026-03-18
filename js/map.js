ce504128551023c44765d422a18967c864eedd43
Paul Coulter <paulhcoulter@gmail.com>
2026-03-17 22:10:56 -0400

    fix: switch to CartoDB dark tiles (no API key required)

--- js/map.js
+++ js/map.js
@@ -48,8 +48,9 @@ function initMap() {
     zoomControl: true
   });
 
-  L.tileLayer("https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png", {
-    attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  L.tileLayer("https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png", {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    subdomains: "abcd",
     maxZoom: 20
   }).addTo(map);
 
